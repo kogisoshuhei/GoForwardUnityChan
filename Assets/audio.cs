65d1b632f8f4a311c228d6ad50844f813321945f
shuhei kogiso <doodles9150@gmail.com>
2019-05-01 12:04:49 +0900

    GoForwardUnityChan 初めてのコミット
    Lesson7の課題データです。

---
+++ Assets/audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour {
+
+	//サウンド設定
+	public AudioClip sound01;
+	
+	void OnCollisionEnter2D (Collision2D collision){
+
+		if ( collision.gameObject.tag == "UnityChanTag" ){
+
+			//Cube衝突時にボリュームを0にする
+			GetComponent<AudioSource>().volume = 0; 
+
+		} else {
+
+			AudioSource.PlayClipAtPoint(sound01, transform.position);
+
+		}
+
+	}
+
+}
